ef33f41a393d110e7386d1cd17326241ca414dee
carla-tvg <ct.valderrama25@gmail.com>
2024-09-24 12:58:34 -0500

    subi los cambios de la pagina nosotros

--- script.js
+++ script.js
@@ -1,9 +1,8 @@
-function openNav() {
-    document.getElementById("sidebar").style.width = "250px";
-    document.getElementById("main").style.marginLeft = "250px";
-}
-function closeNav() {
-    document.getElementById("sidebar").style.width = "0";
-    document.getElementById("main").style.marginLeft = "0";
-}
+
+const mobileMenu = document.getElementById('mobile-menu');
+const navLinks = document.querySelector('.nav-links');
+
+mobileMenu.addEventListener('click', () => {
+  navLinks.classList.toggle('active');
+});
 
